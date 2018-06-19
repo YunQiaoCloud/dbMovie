d6c67cc55edd388954edac1326d4ef0147b95374
Sakuoz <sakuoz@163.com>
2018-06-19 17:43:28 +0800

    完成搜索样式

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="movie-search">
     <div class="search-input">
-      <a href="/pages/movie/main">
-        <input type="text" placeholder="搜索" v-model="searchValue">
-      </a>
+      <input type="text" placeholder="请输入要搜索的电影" v-model="searchValue">
     </div>
 
     <div class="search-result"
          v-for="movie in searchResult"
          :key="movie.id">
-
-      <p>{{ movie.title }}</p>
+      <div class="search-poster">
+        <img :src="movie.images.small">
+      </div>
+      <div class="search-info">
+        <div class="search-title">
+          <p>{{ movie.title }}</p>
+        </div>
+        <div class="search-mark">
+          <span>{{ movie.rating.average }}</span>
+        </div>
+        <div class="search-details">
+          <a :href="'/pages/details/main?id=' + movie.id">
+            <button type="button" name="button">点击查看</button>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -29,14 +41,11 @@ export default {
   },
   computed: {
     searchResult() {
-      console.log(this.$store.state.search.searchResult, 'asd')
       return this.$store.state.search.searchResult
     }
   },
   watch: {
     searchValue: _.debounce(function movieSearch(value) {
-      console.log(value)
-
       this.$store.dispatch('getSearchMovie', value)
     }, 1000)
   }
@@ -44,5 +53,69 @@ export default {
 </script>
 
 <style lang="scss">
+  .movie-search {
+    background-color: #ebebeb;
+  }
 
+  .search-input {
+    border-bottom: 5px solid #f5f5f5;
+
+    input {
+      width: 100%;
+      padding: 5px 10px;
+      background-color: #fff;
+    }
+  }
+
+  .search-result {
+    width: 90%;
+    height: 90px;
+    background-color: #fff;
+    padding: 10px;
+    margin: 15px auto;
+    border-radius: 10px;
+  }
+
+  .search-poster {
+    width: 20%;
+    height: 100%;
+    float: left;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .search-info {
+    width: calc(80% - 20px);
+    margin-left: 20px;
+    float: left;
+  }
+
+  .search-title {
+    width: 100%;
+    font-size: 18px;
+  }
+
+  .search-mark {
+    width: 100%;
+    margin: 10px 0;
+    font-size: 16px;
+    color: #FF2E2E;
+    font-style: oblique;
+  }
+
+  .search-details {
+    width: 50%;
+
+    button {
+      width: 100%;
+      height: 25px;
+      font-size: 12px;
+      line-height: 25px;
+      color: #fff;
+      background-color: #44bb57;
+    }
+  }
 </style>
